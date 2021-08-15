710a597bec298c3ccbad3f14ddfb2f3fae82a580
mmorrisonlk <mmorrisonlk@gmail.com>
2021-08-15 18:09:38 -0500

    Pulled my previous code to make a server that does nothing yet

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const path = require('path')
+const mongoose = require("mongoose");
+const routes = require("./routes");
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'static')))
+app.use(routes);
+
+mongoose.connect(
+    process.env.MONGODB_URI || "mongodb://localhost:27017/fitnessTracker",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    }
+  );
+
+app.listen(PORT, () =>
+    console.log(`API Server now listening on PORT ${PORT}!`)
+)
